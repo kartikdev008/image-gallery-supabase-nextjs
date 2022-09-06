3f9dbae7881ca14f0dd1c33b0f18d61fa3dd92c5
Kartik Shah <kartikdev008@gmail.com>
2022-09-06 14:34:49 +0300

    console logging ISR

--- pages/api/revalidate.js
+++ pages/api/revalidate.js
@@ -1,12 +1,15 @@
 export default async function handler(req, res) {
   if (req.query.secret !== process.env.REVALIDATE_SECRET) {
     return res.status(401).json({ message: "Invalid token" });
+    console.log(res);
   }
 
   try {
     await res.revalidate("/");
+    console.log(res);
     return res.json({ revalidated: true });
   } catch (err) {
+    console.log(res);
     return res.status(500).send("Error revalidating");
   }
 }
